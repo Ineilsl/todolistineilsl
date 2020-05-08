3cc448cc95b901e2762c51bd15301e0130c93ff3
Neils <sergioloscast@gmail.com>
2020-05-08 16:48:25 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 WPDOWNLOADED=/var/tmp/wordpress
+MYSQLDUMP=/var/tmp/database
 WPPATH=/var/www/html
 APA2PATH=/etc/apache2
 mkdir $WPDOWNLOADED
 echo "Configuring Mysql users..."
 service mysql start
-mysql -u root -e "SET PASSWORD FOR root@localhost = PASSWORD('testing')";
+#mysql -u root -e "SET PASSWORD FOR root@localhost = PASSWORD('testing')";
+mysql -u root -p < $MYSQLDUMP/database.db
 echo "Mysql Root User: [OK]"
 echo "Configuring to latest Worpress site..."
 curl -l https://raw.githubusercontent.com/Ineilsl/todolistineilsl/master/wordpress/index.php > $WPDOWNLOADED/index.php

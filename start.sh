9b1d078f15446447a7bd4606c55ffe2991290c76
Neils <sergioloscast@gmail.com>
2020-05-08 16:33:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Starting Mysql..."
 service mysql start
+echo "Mysql [OK]"
+echo "Starting Apache..."
 service apache2 start
+echo "Apache2 [OK]"
 echo "Container has been initialized!"

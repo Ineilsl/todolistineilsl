fc1180ee2444bed85d2b9bf757bfffcd0d14a196
Neils <sergioloscast@gmail.com>
2020-05-08 16:32:13 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,6 @@
 WPDOWNLOADED=/var/tmp/wordpress
 WPPATH=/var/www/html
 APA2PATH=/etc/apache2
-mysqlU=RootPasswd
 mkdir $WPDOWNLOADED
 echo "Configuring Mysql users..."
 service mysql start
@@ -15,5 +14,3 @@ rm -rf $WPPATH/*
 mv $WPDOWNLOADED/index.php $WPPATH
 sed -i 's/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/' $APA2PATH/mods-enabled/dir.conf
 echo "Setup wordpress site [OK]"
-service apache2 start
-echo "Container has been initialized!"

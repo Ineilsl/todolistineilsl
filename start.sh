92b9edd246db0037ca013f153baca8d8055608a9
Neils <Neils@artemisa>
2020-12-02 00:45:42 +0100

    asd

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting Mysql..."
-service mysql start
-echo "Mysql [OK]"
-echo "Starting Apache..."
-service apache2 start
-echo "Apache2 [OK]"
-echo "Container has been initialized!"
